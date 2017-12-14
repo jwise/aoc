8144cdb06431662cdb4b2dbcec12b2e036a8e229
Joshua Wise <joshua@joshuawise.com>
2017-12-14 00:18:39 -0500

    indent 14

--- 2017/14.lua
+++ 2017/14.lua
@@ -2,63 +2,61 @@
 
 
 function knothash(str)
-LEN=256
-
-elts = {}
-for i=0,(LEN-1) do elts[i] = i end
-
-pos = 0
-skips = 0
-
-function mangle(num)
-	startp = pos
-	endp = startp + num - 1
+	LEN=256
 	
-	orig = {}
-	for pp=startp,endp do
-		table.insert(orig, elts[pp % LEN])
-	end
-	origp = num
-	for pp=startp,endp do
-		elts[pp % LEN] = orig[origp]
-		origp = origp - 1
+	elts = {}
+	for i=0,(LEN-1) do elts[i] = i end
+	
+	pos = 0
+	skips = 0
+	
+	function mangle(num)
+		startp = pos
+		endp = startp + num - 1
+		
+		orig = {}
+		for pp=startp,endp do
+			table.insert(orig, elts[pp % LEN])
+		end
+		origp = num
+		for pp=startp,endp do
+			elts[pp % LEN] = orig[origp]
+			origp = origp - 1
+		end
+		
+		pos = pos + num + skips
+		
+		skips = skips + 1
 	end
 	
-	pos = pos + num + skips
+	seq = {}
+	for i in str:gmatch(".") do
+		table.insert(seq, i:byte())	
+	end
+	table.insert(seq, 17)
+	table.insert(seq, 31)
+	table.insert(seq, 73)
+	table.insert(seq, 47)
+	table.insert(seq, 23)
 	
-	skips = skips + 1
-end
-
-seq = {}
-for i in str:gmatch(".") do
-	table.insert(seq, i:byte())	
-end
-table.insert(seq, 17)
-table.insert(seq, 31)
-table.insert(seq, 73)
-table.insert(seq, 47)
-table.insert(seq, 23)
-
-for i=0,63 do
-	for k,v in ipairs(seq) do
-		mangle(v)
+	for i=0,63 do
+		for k,v in ipairs(seq) do
+			mangle(v)
+		end
 	end
-end
 
-dens = {}
-denss = ""
-for i=0,15 do
-	xorv = 0
-	for j = 0,15 do
-		xorv = xorv ~ elts[i * 16 + j]
+	dens = {}
+	denss = ""
+	for i=0,15 do
+		xorv = 0
+		for j = 0,15 do
+			xorv = xorv ~ elts[i * 16 + j]
+		end
+		table.insert(dens, xorv)
 	end
-	table.insert(dens, xorv)
+
+	return dens
 end
-
-return dens
-
-end
-
 
 local xpop = 0
 local key = io.read("*line")
