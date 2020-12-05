3f4b0aa5ea1a32680331a25919b7b5faceee64f9
Joshua Wise <joshua@joshuawise.com>
2020-12-04 21:14:19 -0800

    5, but with the obvious insight

--- 2020/5b.lua
+++ 2020/5b.lua
@@ -5,21 +5,14 @@ while true do
 	line = io.read("*line")
 	if not line then break end
 	
+	local sid = 0
 	local rmin,rmax,cmin,cmax = 0,127,0,7
 	for c in line:gmatch(".") do
-		if c == "F" then
-			rmax = (rmax - rmin + 1) / 2 - 1 + rmin
-		elseif c == "B" then
-			rmin = (rmax - rmin + 1) / 2 + rmin
-		elseif c == "L" then
-			cmax = (cmax - cmin + 1) / 2 - 1 + cmin
-		elseif c == "R" then
-			cmin = (cmax - cmin + 1) / 2 + cmin
-		else
-			abort()
+		sid = sid * 2
+		if c == "B" or c == "R" then
+			sid = sid + 1
 		end
 	end
-	local sid = rmin * 8 + cmin
 	if sid > sidmax then
 		sidmax = sid
 	end
