63aee803a582ca8177cfd8b84cdbab2b7f837ae3
Joshua Wise <joshua@joshuawise.com>
2024-12-13 01:07:48 -0500

    13 with a tactical nuke

---
+++ 2024/13b.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env pypy3
+
+from aocd import get_data
+
+from collections import *
+import math
+import re, parse,functools, heapq,itertools
+from z3 import *
+import z3
+
+# re.findall(pattern, string, flags)   r'\s'
+# re.sub(r'<b>(.*?)</b>', r'\1', str)
+# parse.search("age: {:d}\n", "Name: foo\nage: 35\nColor: red")[0] -> 35
+# have you considered: split('\n\n')?
+# have you considered: the walrus operator?
+# have you considered: a tactical nuke on an ant?
+# have you considered: 
+
+def extract(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+d = get_data(year = 2024, day = 13)
+
+apress = Int("apress")
+bpress = Int("bpress")
+
+tot_toks = 0
+for game in d.split('\n\n'):
+    ax, ay, bx, by, px, py = extract(game)
+    px += 10000000000000
+    py += 10000000000000
+    s = Optimize()
+    s.add(ax * apress + bx * bpress == px)
+    s.add(ay * apress + by * bpress == py)
+    tokens = 3 * apress + bpress
+    h = s.minimize(tokens)
+    if s.check() == unsat:
+        continue
+    tot_toks += s.lower(h).as_long()
+
+print(tot_toks)
+        
